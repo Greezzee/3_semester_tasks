fa138ceaab95573f65faffa5e97f081af6c40169
root <zorin.eugene@gmail.com>
2020-09-09 12:24:09 +0300

    String Generator from 2 seminar

---
+++ StringTask/stringgen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+void GenerateString(int n, char* string)
+{
+    char counter = 'a';
+
+    for (int i = 0; i < n; i++)
+    {
+        int len = strlen(string);
+        string[len] = counter;
+
+        for (int j = 0; j < len; j++)
+            string[len + j + 1] = string[j];
+        counter++;
+    }
+
+}
+
+int main()
+{
+    for (int i = 1; i < 10; i++) {
+        char* str = (char*)calloc(pow(2, i) - 1, 1);
+        GenerateString(i, str);
+        printf("%s\n\n", str);
+        free(str);
+    }
+    return 0;
+}
